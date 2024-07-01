464e8d312eaca19aad849ee011d9c0812b02c77b
pawaryash <er.yashpawar@gmail.com>
2024-07-01 18:24:27 +0530

    chore: Add cookieParser middleware to app.js
    This commit adds the `cookieParser` middleware to the `app.js` file in the backend. The `cookieParser` middleware is used to parse cookies in incoming requests. This change enables the application to handle cookies and sets up the necessary functionality for user authentication and session management. Also added user login and logout functionality. in addition with role based authentication and authorization.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -43,3 +43,18 @@ exports.loginUser = catchAsyncError(async (req, res, next)=>{
     sendToken(user,201,res);
 });
 
+//Logout User
+exports.logoutUser = catchAsyncError(async (req, res, next)=>{
+
+    res.cookie("token",null,{
+        expires: new Date(Date.now()),
+        httpOnly: true,
+    })
+
+    res.status(200).json({
+        success: true,
+        message: "User logged out successfully",
+    });
+
+});
+
